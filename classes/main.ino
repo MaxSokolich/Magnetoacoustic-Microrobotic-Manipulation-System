b665c6d73a4829c374fccebe24ce633bbd293999
DasLab325 <DasLab325@MEEG-DMO9472H0Q.win.udel.edu>
2024-02-23 16:13:09 -0500

    not offsetting xyz coil magnitudes

--- classes/main.ino
+++ classes/main.ino
@@ -407,9 +407,9 @@ void loop()
        By_final = magnitude * (By / sqrt(Bx*Bx + By*By + Bz*Bz));
        Bz_final = magnitude * (Bz / sqrt(Bx*Bx + By*By + Bz*Bz));
 
-       Bx_final = Bx_final;
-       By_final = By_final * .75;
-       Bz_final = Bz_final * .5;
+       //Bx_final = Bx_final;
+       //By_final = By_final * .75;
+       //Bz_final = Bz_final * .5;
        
    }
    
